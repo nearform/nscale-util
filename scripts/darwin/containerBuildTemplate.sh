927db16aebf775c86f611f214d72641e7366c8a9
Peter Elger <elger.peter@gmail.com>
2014-09-06 21:12:01 +0100

    updated progress and fixed xargs

--- scripts/darwin/containerBuildTemplate.sh
+++ scripts/darwin/containerBuildTemplate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' | xargs -r docker rm
-docker images --no-trunc| grep none | awk '{print $2}' | xargs -r docker rmi
+docker ps -a --no-trunc | grep 'Exit' | awk '{print $1}' |  xargs -I {} docker rm {}
+docker images -notrunc| grep none | awk '{print $2}' | xargs -I {} docker rmi {}
 docker build -t __NAMESPACE__/__TARGETNAME__-__BUILDNUMBER__ .
 
